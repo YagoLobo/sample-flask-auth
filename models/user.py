fb87037530710cbded4aa3bc226bc426d44da1c0
Yago <yagolobo.dev@gmail.com>
2024-02-23 17:33:08 -0300

    Implementing user roles

--- models/user.py
+++ models/user.py
@@ -3,5 +3,6 @@ from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(50), unique=False, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(100), unique=False, nullable=False)
+    role = db.Column(db.String(80), nullable=False, default='user')
